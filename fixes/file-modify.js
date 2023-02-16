d694bb7b59ed9a4918deea5665f5acc3dd6d50e6
Justin Eveland <jeveland@newrelic.com>
2023-02-16 09:23:10 -0500

    Revert "chore: address lint error (#5)"
    This reverts commit ad0714e82aeb8acb48fe1018626a3e8a5ae2dead.

--- fixes/file-modify.js
+++ fixes/file-modify.js
@@ -116,12 +116,14 @@ async function fileModify(fs, options, targets, dryRun = false) {
       // return the target information
       const message =
         typeof options.text === 'object'
-          ? `${options.write_mode} text from ${
-              options.text.file || options.text.url
-            } to file`
+          ? `${options.write_mode} text from ${options.text.file || options.text.url
+          } to file`
           : `${options.write_mode} \`${JSON.stringify(content)
-              .replace(/\\"/g, '"')
-              .slice(1, -1)}\` to file`
+            .replace(/\\"/g, '"')
+            .slice(
+              1,
+              -1
+            )}\` to file`
       return {
         message,
         passed: true,
